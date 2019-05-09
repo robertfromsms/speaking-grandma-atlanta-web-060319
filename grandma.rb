42447644dc474491f64795310fb058f044ea43c6
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-09 16:22:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,6 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    end
     else
       return "NO, NOT SINCE 1938!"
     end
