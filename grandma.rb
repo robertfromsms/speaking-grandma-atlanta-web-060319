1f3aa282aff81e70f82940a9ff5c203b5143eaf2
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-09 16:21:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,6 +9,7 @@ def speak_to_grandma(phrase)
     end
   else
     return "HUH?! SPEAK UP, SONNY!"
+  end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
